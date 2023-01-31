eec5bba81b1ed84ed17fca1f10f624649fd4a0d9
hamza101muf <hamza101muf@gmail.com>
2023-01-31 07:14:39 -0500

    labs changed/fixed

--- chapter_7_labs/Shop.java
+++ chapter_7_labs/Shop.java
@@ -14,30 +14,35 @@ public class Shop {
         String itemName;
         double itemPrice;
         int quantity;
-        int totalPrice=0;
+        double totalPrice=0;
         Scanner scan = new Scanner(System.in);
         String keepShopping = "y";
         ArrayList <Item> cart = new ArrayList <Item>();
         NumberFormat fmt = NumberFormat.getCurrencyInstance();
 
-        do {
+        while (keepShopping.equals("y")) {
             System.out.print("Enter the name of the item: ");
             itemName = scan.next();
             System.out.print("Enter the unit price: ");
             itemPrice = scan.nextDouble();
             System.out.print("Enter the quantity: ");
             quantity = scan.nextInt();
+            totalPrice += (itemPrice*(double)quantity);
             // *** create a new item and add it to the cart
             cart.add(new Item(itemName, itemPrice, quantity));
             // *** print the contents of the cart object using println
-            System.out.println(cart);
+            System.out.println("Name\tQuantity\tPrice\tTotal");
+            for (int i=0;i<cart.size();i++) {
+                System.out.println(cart.get(i).toString());
+            }
+            System.out.println("Your current total bill is " + fmt.format(totalPrice));
             System.out.print("Continue shopping (y/n)? ");
             keepShopping = scan.next();
         }
-        while (keepShopping.equals("y"));
-        for (int i=0;i<cart.size();i++) {
-            totalPrice += cart.get(i).getPrice();
-        }
+        
+        /*for (int i=0;i<cart.size();i++) {
+            totalPrice += (cart.get(i).getPrice()*(double)(cart.get(i).getQuantity()));
+        }*/
         System.out.println("Your total bill is " + fmt.format(totalPrice));
 
         scan.close();
---
+++ chapter_7_labs/Test.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Test {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(2,4);
+        list.set(2,3);
+
+
+
+
+
+
+        System.out.println(list);
+    }
+}
